4f9932b05ac49e69773d3e725b8806dc4bcda451
Mohanad Al-Abiad <weiss97m@gmail.com>
2022-04-25 17:10:20 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/gamingbud/GamingbudApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.gamingbud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingbudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingbudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/gamingbud/GamingbudApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.gamingbud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingbudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
